a7d46c9146e58f7d5972413856b857626d9ac548
maksymg <mgnyniuk@gmail.com>
2013-12-14 20:07:14 +0200

    add Configuration class

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,37 @@
+import com.gpusim2.config.GridSimConfig;
+
+import java.beans.XMLEncoder;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by maksym on 12/14/13.
+ */
+public class Configuration implements Callable<Boolean>, Serializable {
+
+    private List<GridSimConfig> gridSimConfigList;
+
+    public Configuration() { }
+
+    public Configuration(List<GridSimConfig> gridSimConfigList) {
+        this.gridSimConfigList = gridSimConfigList;
+    }
+
+    public Boolean call() throws IOException {
+        int i = 0;
+        for (GridSimConfig gridSimConfig : gridSimConfigList) {
+            FileOutputStream out = new FileOutputStream("config" + i + ".xml");
+            XMLEncoder xmlEncoder = new XMLEncoder(out);
+            xmlEncoder.writeObject(gridSimConfig);
+            xmlEncoder.flush();
+            xmlEncoder.close();
+
+            i++;
+        }
+
+        return true;
+    }
+}
