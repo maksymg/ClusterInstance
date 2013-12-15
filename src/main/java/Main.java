933d3315a1091f6588c25d27f9d3ec357f7bdf5e
maksymg <mgnyniuk@gmail.com>
2013-12-15 19:43:49 +0200

    parallel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,11 @@ import com.hazelcast.core.HazelcastInstance;
 /**
  * Created by maksym on 12/14/13.
  */
-public class MainClass {
+public class Main {
 
     public static void main(String[] args) {
         Config cfg = new Config();
+        cfg.setProperty("hazelcast.icmp.timeout", "2000000000");
         Hazelcast.newHazelcastInstance(cfg);
     }
 }
